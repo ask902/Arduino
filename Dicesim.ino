38bce2a4edebd1c0d238a4e0fdc579ffe9abd9be
ask902 <43297588+ask902@users.noreply.github.com>
2019-01-19 14:59:05 -0500

    Add files via upload

---
+++ Dicesim.ino
@@ -0,0 +1,50 @@
+/* 
+ Arduino Dice :)
+ This example shows how to simulate throwing a dice with 6 LEDs
+ By Esther van der Stappen
+ */
+
+// 6 consecutive digital pins for the LEDs
+int first=13;
+int second=12;
+int third=11;
+int fourt=10;
+int fifth=9;
+int sixth=8;
+
+// pin for the button switch
+int button;
+// value to check state of button switch
+int pressed;
+
+void setup() {
+  // set all LED pins to OUTPUT
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  // set buttin pin to INPUT
+
+  // initialize random seed by noise from analog pin 0 (should be unconnected)
+  randomSeed(analogRead(0))
+ 
+}
+
+void loop() {
+  // if button is pressed - throw the dice
+  pressed;
+  
+  if (pressed == HIGH) {
+    // remove previous number to LOW
+    int randomNum=random(1,7);
+    // get a random number in the range [1,6]
+    if(randomNum>=1){
+    //write IF statements to light up the lights
+    digitalWrite(first, HIGH);    
+  } if(randomNum>=2{
+    digitalWrite(second, HIGH);
+  }
+  
+}
