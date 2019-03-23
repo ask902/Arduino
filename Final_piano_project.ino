05e6ed18d21edf797852833ad24f07e18cb523f2
ask902 <43297588+ask902@users.noreply.github.com>
2019-03-23 14:57:44 -0400

    Add files via upload

---
+++ Final_piano_project.ino
@@ -0,0 +1,41 @@
+#include "pitches.h"
+
+//Pick pins for the Buzzer 
+pinMode(3,OUTPUT)
+pinMode(4,OUTPUT)
+//pick pins for the Piano Buttons
+pinMode(2,OUTPUT)
+pinMode(3,OUTPUT)
+pinMode(4,OUTPUT)
+
+void setup()
+{
+  Serial.begin(9600);
+  //Initialize Buttons
+}
+
+
+// notes in the melody:
+int melody[] = {
+//use preset pitches to make a song
+
+
+
+
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  
+//set duration of notes
+
+};
+
+
+void loop()
+{
+  //while Button is pressed, play set tone
+  //do this for all buttons
+  //else play no tone
+
+}
